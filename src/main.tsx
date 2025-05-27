968d48f9c9c5e38b625c4af910286eae1283dedd
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2025-05-27 17:32:28 +0530

    d

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,24 @@
-import { createRoot } from 'react-dom/client'
+/*import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);*/
+
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import "./index.css";
+ 
+ ReactDOM.createRoot(document.getElementById("root")!).render(
+   <React.StrictMode>
++    <BrowserRouter basename="/SAMS">
++      <Routes>
++        <Route path="/*" element={<App />} />
++      </Routes>
+     </BrowserRouter>
+   </React.StrictMode>
+ );
+
+// Path: src\main.tsx
+
