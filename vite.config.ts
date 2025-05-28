bbc6548c00ee907871258088abf0ed730414fe0e
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2025-05-28 14:20:37 +0530

    build and deploy error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from 'path'
 
 // https://vitejs.dev/config/
 // 👇 Add your repo name here
 export default defineConfig({
-  base: '/SAMS/', 
+  base: '/SAMS/', // make sure this is your repo name
   plugins: [react()],
-  build: {
-    outDir: 'build',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
