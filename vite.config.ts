b7b3821561bef77f857ca935068d4c86add49d1a
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2025-05-28 14:16:22 +0530

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+// 👇 Add your repo name here
+export default defineConfig({
   base: '/SAMS/', 
-  server: {
-    host: "::",
-    port: 8080,
+  plugins: [react()],
+  build: {
+    outDir: 'build',
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+})
