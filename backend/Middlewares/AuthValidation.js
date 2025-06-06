1ef43f3a3a8bb6f1c3e6f587ac44dc8a8f03494a
Praful-Daksh <prafuldaksh@proton.me>
2025-06-06 23:24:59 +0530

    Added backend register route

---
+++ backend/Middlewares/AuthValidation.js
@@ -0,0 +1,88 @@
+const joi = require('joi');
+
+const baseSchema = {
+    name: joi.string().trim().min(2).max(100).required(),
+    email: joi.string().email().lowercase().required(),
+    password: joi.string().required(),
+    phoneNumber: joi.string().pattern(/^[0-9]{10}$/).required(),
+    role: joi.string().valid('Student', 'Admin', 'Faculty', 'HOD', 'ClassCoordinator').required()
+};
+
+
+const studentSchema = joi.object({
+    ...baseSchema,
+    aparId: joi.string().trim().required(),
+    admissionDate: joi.date().max('now').required(),
+    currentYear: joi.number().required(),
+    semester: joi.number().required(),
+    department: joi.string().required(),
+    transport: joi.string().required(),
+    parentPhone: joi.string().pattern(/^[0-9]{10}$/).required(),
+    address: joi.string().required()
+});
+
+const facultySchema = joi.object({
+    ...baseSchema,
+    department: joi.string().required(),
+});
+
+const hodSchema = joi.object({
+    ...baseSchema,
+    department: joi.string().required()
+});
+
+const classCoordinatorSchema = joi.object({
+    ...baseSchema,
+    name: joi.string().required()
+});
+
+const adminSchema = joi.object({
+    ...baseSchema,
+    department: joi.string().required()
+});
+
+const registerValidation = (req, res, next) => {
+    try {
+        const { role } = req.body;
+        let schema;
+        if (role == 'Student') {
+            schema = studentSchema;
+        } else if (role == 'Faculty') {
+            schema = facultySchema;
+        } else if (role == 'HOD') {
+            schema = hodSchema;
+        } else if (role == 'ClassCoordinator') {
+            schema = classCoordinatorSchema;
+        } else if (role == 'Admin') {
+            schema = adminSchema;
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: 'Role is not Valid'
+            });
+        }
+
+        const { error } = schema.validate(req.body)
+
+        if (error) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid data',
+                error: error.details[0].message
+            });
+        }
+        next();
+
+    } catch (err) {
+        console.error('Validation error:', err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error during validation',
+        });
+    }
+};
+
+
+module.exports = {
+    registerValidation
+};
---
+++ backend/Models/User.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose');
+const options = { discriminatorKey: 'role', collection: 'users' };
+
+
+const baseUserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['Student', 'Admin', 'Faculty', 'HOD', 'ClassCordinator']
+    }
+}, options);
+
+const User = mongoose.model('User', baseUserSchema);
+
+const studentSchema = new mongoose.Schema({
+    aparId: {
+        type: String,
+        required: true
+    },
+    admissionDate: {
+        type: Date,
+        required: true
+    },
+    currentYear: {
+        type: Number,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    transport: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    parentPhone: {
+        type: String,
+        required: true
+    },
+});
+
+const Student = User.discriminator('Student', studentSchema);
+
+
+const adminSchema = new mongoose.Schema({
+    department: {
+        type: String,
+        required: true
+    }
+});
+
+const Admin = User.discriminator('Admin', adminSchema);
+
+
+const facultySchema = new mongoose.Schema({
+    department: {
+        type: String,
+        required: true
+    },
+});
+
+const Faculty = User.discriminator('Faculty', facultySchema);
+
+
+const hodSchema = new mongoose.Schema({
+    department: {
+        type: String,
+        required: true
+    }
+});
+
+const HOD = User.discriminator('HOD', hodSchema);
+
+
+const classCoordinatorSchema = new mongoose.Schema({
+    department: {
+        type: String,
+        required: true
+    }
+});
+
+const ClassCoordinator = User.discriminator('ClassCordinator', classCoordinatorSchema);
+
+
+module.exports = {
+    User,
+    Student,
+    Admin,
+    Faculty,
+    HOD,
+    ClassCoordinator,
+};
