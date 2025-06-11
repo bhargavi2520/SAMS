ecc310be5db14ae3f5baf8514a9017afee236900
Praful Daksh <prafuldaksh@proton.me>
2025-06-11 13:48:13 +0530

    reverted the change of local url to env

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // Get the backend URL from environment variables
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
