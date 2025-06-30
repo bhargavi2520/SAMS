2bf8aca59cf3a59f0e6e316e8e6acad15bc08bf5
Praful Daksh <prafuldaksh@proton.me>
2025-06-30 18:45:22 +0530

    Reapply ""
    This reverts commit f6dc082e5ac61e62df0d97d6e0ab641d25ee309a.

--- backend/Routes/attendanceRouter.js
+++ backend/Routes/attendanceRouter.js
@@ -4,17 +4,23 @@ const {
   getAttendancebySubject,
   markAttendance,
 } = require("../Controllers/AttendanceController");
+const {
+  getAttendanceBySubjectValidation,
+  markAttendanceValidation,
+} = require("../Middlewares/AttendanceValidation");
 const AttendanceRouter = express.Router();
 
 AttendanceRouter.get(
   "/attendancebySubject",
   ensureAuthenticated([]),
+  getAttendanceBySubjectValidation,
   getAttendancebySubject
 );
 
 AttendanceRouter.post(
   "/mark",
   ensureAuthenticated(["FACULTY"]),
+  markAttendanceValidation,
   markAttendance
 );
 
